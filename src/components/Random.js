9ed04a2bd33c1035186a969f9a863cb7cee2c64b
afolabi mabunmi <brownboy3047@gmail.com>
2023-03-17 20:08:27 +0100

    initial commit

---
+++ src/components/Random.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import "./Random.css";
+
+const Random = () => {
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [inputNumber, setInputNumber] = useState(0);
+
+  const getRandomNumber = () => {
+    setRandomNumber(Math.floor(Math.random() * `${inputNumber}`));
+  };
+
+  const handleChange = (e) => {
+    setInputNumber(e.target.value);
+  };
+
+  return (
+    <div className="random">
+      <p>Random Number: {randomNumber}</p>
+      <p>
+        A random number will be generated between 0 and the number you entered
+        below
+      </p>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={inputNumber}
+        placeholder="enter number"
+      />
+      <button onClick={getRandomNumber} className="generate">
+        Random Number
+      </button>
+    </div>
+  );
+};
+
+export default Random;
